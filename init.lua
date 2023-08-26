008ee8a3a6da170ab727f22baa988fbf88283f1b
ox1david <bousi.david@gmail.com>
2023-08-26 22:16:25 +0200

    indent spacing

--- init.lua
+++ init.lua
@@ -10,6 +10,13 @@ require("plugins.cmp")
 require("plugins.telescope")
 require("plugins.gitsigns")
 require("plugins.autopairs")
+
+-- Indent Size
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- Relative Numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
