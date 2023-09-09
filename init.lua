45b37bd00e074af71ebaf20db822c2a05b62f732
ox1david <bousi.david@gmail.com>
2023-09-09 22:37:04 +0200

    Added Harpoon and UndoTree, adjusted settings

--- init.lua
+++ init.lua
@@ -8,8 +8,9 @@ require("plugins.treesitter")
 require("plugins.cmp")
 require("plugins.telescope")
 require("plugins.gitsigns")
---require("plugins.autopairs")
 require("plugins.pairs")
+require("plugins.harpoon")
+require("plugins.undotree")
 -- Indent Size
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
@@ -26,9 +27,6 @@ vim.o.mouse ='a'
 -- Yank to System Clipboard
 vim.opt.clipboard = "unnamedplus"
 
--- Save Undo History
-vim.opt.undofile = true
-
 -- Case Insensitive Searching
 vim.o.ignorecase = true
 vim.o.smartcase = true
@@ -38,3 +36,18 @@ vim.opt.termguicolors = true
 vim.g.sonokai_style = 'atlantis'
 vim.g.sonokai_better_performance = 1 
 vim.cmd("colorscheme sonokai")
+
+-- Replace Swap Files With UndoTree
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Search Highlighting
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- QOL
+vim.opt.wrap = false -- Linewrap
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,42 +1,67 @@
 ----------------------------------------------------------------------------------------
--- LAZY 
----------------------------------------------------------------------------------------
+-- LAZY
+----------------------------------------------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- Leader
+-- Leader Mapping (Required by Lazy)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
 ----------------------------------------------------------------------------------------
 -- PLUGINS
 ----------------------------------------------------------------------------------------
 local plugins = {
-  'wbthomason/packer.nvim',
   'tpope/vim-fugitive',
-  'tpope/vim-commentary',
   'tpope/vim-surround',
-  { 'nvim-telescope/telescope.nvim', dependencies = {'nvim-lua/plenary.nvim' }},
-  "sainnhe/sonokai",
-  { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true }},
-  { 'lewis6991/gitsigns.nvim', dependencies = { 'nvim-lua/plenary.nvim' }},
   'nvim-treesitter/nvim-treesitter-textobjects',
   'hrsh7th/cmp-nvim-lsp',
   'saadparwaiz1/cmp_luasnip',
   'L3MON4D3/LuaSnip',
   'echasnovski/mini.pairs',
- {
+  'theprimeagen/harpoon',
+  'mbbill/undotree',
+  {
+    'tpope/vim-commentary',
+    event = "VeryLazy" 
+    },
+
+  { 
+    'nvim-telescope/telescope.nvim',
+    dependencies = {'nvim-lua/plenary.nvim' 
+        }
+    },
+
+  { 
+    'sainnhe/sonokai',
+    lazy = true 
+    },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+  },
+
+  { 
+    'lewis6991/gitsigns.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' 
+        }
+    },
+
+  {
     "hrsh7th/nvim-cmp",
-    -- event = 'InsertEnter',
     event = 'VeryLazy',
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
@@ -55,12 +80,13 @@ local plugins = {
   },
 }
 
--- Mods
-
+-- Lazy Initialize
 require("lazy").setup(plugins, {})
+
+-- Lualine setup
 require('lualine').setup {
-        options = {
-        theme = 'sonokai'
-          }
-        }
+  options = {
+    theme = 'sonokai'
+  }
+}
 
